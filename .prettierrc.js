090f48e0745b0a41cc8201dde8e7eabeb4908400
erikpodusel <57442374+erikpodusel@users.noreply.github.com>
2022-11-11 08:33:03 +0100

    Create .prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  singleQuote: false,
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  quoteProps: 'as-needed',
+  printWidth: 100,
+};
