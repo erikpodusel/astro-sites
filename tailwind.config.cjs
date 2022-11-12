f2096b3efd067240720a36f50a6ec794e05cda1b
Erik Podušel <podusel@gomerch.company>
2022-11-12 08:05:50 +0100

    fixed tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,14 +2,14 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		screens: {
-			xs: "576px"
-		},
 		extend: {
+			screens: {
+				xs: "576px"
+			},
 			colors: {
 				primary: "orange",
 				secondary: "#000",
-				terciary: "#000",
+				tertiary: "#000",
 			}
 		},
 	},
