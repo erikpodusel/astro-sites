79c0c9c2dd769fab6324c33f54ff7aff03680510
Erik Podušel <podusel@gomerch.company>
2022-11-13 12:01:45 +0100

    added menu animations pages for menu button

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -9,6 +9,7 @@ interface IMenuItem {
 export const Menu: FC = () => {
   const cacheKey = "menu-collapse";
   const [collapsed, setCollapsed] = useState(true);
+  const [pathname, setPathname] = useState("");
 
   const menuItems: IMenuItem[] = [
     {
@@ -28,9 +29,18 @@ export const Menu: FC = () => {
     },
   ];
 
+  const paragraphStyle = `${collapsed ? "w-0 opacity-0" : "w-full opacity-100"} transition-all duration-300 overflow-clip`;
+
   const handleCollapse = () => {
-    setCollapsed((prev) => !prev);
+    setCollapsed((prev) => {
+      localStorage.setItem(cacheKey, JSON.stringify(!prev));
+
+      return !prev});
   };
+
+  useEffect(() => {
+    setPathname(window.location.pathname)
+  }, []);
 
   useEffect(() => {
     const cachedValue = localStorage.getItem(cacheKey);
@@ -40,10 +50,6 @@ export const Menu: FC = () => {
     setCollapsed(JSON.parse(cachedValue));
   }, []);
 
-  useEffect(() => {
-    localStorage.setItem(cacheKey, JSON.stringify(collapsed));
-  }, [collapsed]);
-
   return (
     <section
       className={`z-50 max-h-screen ${
@@ -51,27 +57,29 @@ export const Menu: FC = () => {
       } flex flex-col m-4 card transition-all duration-300 select-none`}
     >
       <span
-        className={`flex h-12 hover:bg-gray-100 transition-all items-center rounded-xl duration-300 cursor-pointer ${
+        className={`flex h-12 hover:bg-gray-100 transition-all items-center hover:rounded-3xl duration-300 cursor-pointer ${
           collapsed ? "w-12" : "w-full"
         }`}
         onClick={handleCollapse}
       >
         <img src="menu.png" alt="Menu burger" className={"h-6 w-6 mx-3"} />
-        <p className={`${collapsed ? "w-0" : "w-full"} transition-all duration-300 overflow-clip`}>
+        <p className={paragraphStyle}>
           Zavrieť
         </p>
       </span>
       {menuItems.map(({ title, icon, to }) => (
         <a
-          className={`flex h-12 hover:bg-gray-100 transition-all items-center rounded-xl duration-300 ${
+          className={`flex h-12 hover:bg-gray-100 transition-all items-center hover:rounded-3xl duration-300 ${
             collapsed ? "w-12" : "w-full"
-          }`}
+          } ${pathname?.includes(to) ? 'bg-gray-100 rounded-xl' : ''}`}
           href={to}
           key={title}
         >
           {icon}
           <p
-            className={`${collapsed ? "w-0" : "w-full"} transition-all duration-300 overflow-clip`}
+            className={
+              paragraphStyle
+            }
           >
             {title}
           </p>
