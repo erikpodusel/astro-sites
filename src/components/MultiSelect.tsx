82cd1a9dcb111df70b3903f093ab824da7e9123c
Erik Podušel <podusel@gomerch.company>
2022-12-25 23:48:46 +0100

    fixed multiselect not keeping order in which items were selected

--- src/components/MultiSelect.tsx
+++ src/components/MultiSelect.tsx
@@ -1,5 +1,5 @@
 import { Popover } from "@headlessui/react";
-import { FC, useState } from "react";
+import { FC, ReactNode, useCallback, useState } from "react";
 import { Tag } from "./Tag";
 
 interface MultiSelectItem {
@@ -15,27 +15,13 @@ interface Props {
   data?: MultiSelectItem[];
 }
 
-const panelData: MultiSelectItem[] = [
-  // {
-  //   value: "1",
-  //   title: "Product 1",
-  // },
-  // {
-  //   value: "2",
-  //   title: "Product 2",
-  // },
-  // {
-  //   value: "3",
-  //   title: "Product 3",
-  // },
-];
+const panelData: MultiSelectItem[] = [];
 
 for (let i = 0; i < 1000; i++) {
   panelData.push({ value: i.toString(), title: `Product ${i}` });
 }
 
 export const MultiSelect: FC<Props> = ({ placeholder, defaultValue, data = panelData }) => {
-  // export const MultiSelect: FC = ({}) => {
   const [selectedValues, setSelectedValues] = useState<Set<string>>(new Set(defaultValue));
 
   const handleClick = (value: string) => {
@@ -52,23 +38,35 @@ export const MultiSelect: FC<Props> = ({ placeholder, defaultValue, data = panel
     });
   };
 
+  const renderTags = useCallback(() => {
+    if (!selectedValues.size) return placeholder;
+
+    const tags: ReactNode[] = [];
+
+    selectedValues.forEach((selectedValue) => {
+      const title = data.find(({ value }) => value === selectedValue)?.title;
+
+      if (!title) return;
+
+      tags.push(
+        <Tag
+          title={title}
+          onClick={(e) => {
+            e.preventDefault();
+            handleClick(selectedValue);
+          }}
+          key={`tag-${title}-${selectedValue}`}
+        />,
+      );
+    });
+
+    return tags;
+  }, [selectedValues, data, placeholder]);
+
   return (
     <Popover className="relative">
       <Popover.Button className={"input items-center w-full row flex-wrap gap-2"}>
-        {selectedValues.size > 0
-          ? data.map(({ value, title }) =>
-              selectedValues.has(value) ? (
-                <Tag
-                  title={title}
-                  onClick={(e) => {
-                    e.preventDefault();
-                    handleClick(value);
-                  }}
-                  key={`tag-${title}-${value}`}
-                />
-              ) : null,
-            )
-          : placeholder}
+        {renderTags()}
       </Popover.Button>
       <Popover.Panel className="absolute z-10 max-h-60 col card gap-0 overflow-scroll">
         {data.map(({ value, title }, index) => (
