3ce7ab4cfde14c40b43acfbd921f12490b0e4702
erikpodusel <erik.podusel@gmail.com>
2023-04-10 11:54:22 +0200

    feat: added vercel serverless adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
 import astroI18next from 'astro-i18next'
-
+import vercelServerless from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   integrations: [tailwind(), react(), astroI18next()],
+  adapter: vercelServerless(),
 })
