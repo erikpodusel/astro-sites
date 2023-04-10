739bf950a9c0317d067799b2cc580560f9c4e081
erikpodusel <erik.podusel@gmail.com>
2023-04-10 12:56:46 +0200

    fix: changed runtime versioon

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,25 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       screens: {
-        xs: "576px",
-        "3xl": "1920px",
+        xs: '576px',
+        '3xl': '1920px',
       },
       colors: {
-        primary: "orange",
-        secondary: "#000",
-        tertiary: "#000",
-        hover: "#f3f4f6",
+        primary: 'orange',
+        secondary: '#000',
+        tertiary: '#000',
+        hover: '#f3f4f6',
       },
       boxShadow: {
-        light: "rgba(90, 114, 123, 0.11) 0px 7px 30px 0px",
+        light: 'rgba(90, 114, 123, 0.11) 0px 7px 30px 0px',
       },
       fontFamily: {
-        montserrat: "Montserrat, sans-serif",
+        montserrat: 'Montserrat, sans-serif',
       },
     },
   },
   plugins: [],
-};
+}
